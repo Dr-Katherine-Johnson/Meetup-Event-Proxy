2cfdd75efb286458a4237b6aaad24cf8600350d1
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2020-02-22 15:31:19 -0500

    Adds comments

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+// instead of serving a prewritten index.html file with express.static
+// create a route that handles GET requests to the root url
+  // in that route handler
+    // download the bundle(s) from any service that this proxy is going to SSR // TODO: does this happen in the route??
+    // create the HTML markup - the layout - that those bundles and going to render
+      // insert that markup as strings into the layout
+    // prepare the HTML string that will be sent back to the client
+    // send that string as a response to the initial page load
